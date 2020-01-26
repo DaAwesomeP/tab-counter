816643c7a8e75104a18d5f0169a4ad25f3463af2
Tilwa Qendov <tilwa.qendov@gmail.com>
2020-01-26 22:55:22 +0100

    Use exports instead of gulp.task

--- gulpfile.js
+++ gulpfile.js
@@ -28,70 +28,97 @@ const rename = require('gulp-rename')
 const sourcemaps = require('gulp-sourcemaps')
 const zip = require('gulp-zip')
 
-gulp.task('check', () => {
+function lint () {
   return gulp.src(['src/**/*.js', 'gulpfile.js'])
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError())
-})
+}
+lint.description = 'Run eslint and fail on error'
+exports.lint = lint
 
-gulp.task('checkSafe', () => {
+function lintSafe () {
   return gulp.src(['src/**/*.js', 'gulpfile.js'])
     .pipe(eslint())
     .pipe(eslint.format())
-})
+}
+lintSafe.description = 'Run eslint'
+exports.lintSafe = lintSafe
 
-gulp.task('static', () => {
+function trim () {
   return gulp.src('src/**/*.js')
     .pipe(lec())
     .pipe(gulp.dest('src'))
-})
+}
+trim.description = 'Fix line endings'
+exports.trim = trim
 
-gulp.task('clean', (callback) => {
-  del(['dist/*', 'build/*']).then(() => {
-    callback()
-  })
-})
+function clean (callback) {
+  del(['dist/*', 'build/*'])
+    .then(() => { callback() })
+}
+clean.description = 'Remove build artifacts'
+exports.clean = clean
 
-gulp.task('compile', gulp.parallel(() => {
-  return gulp.src('src/**/*.js')
-    .pipe(sourcemaps.init())
-    .pipe(bro({
-      transform: [
-        babelify.configure()
-      ]
-    }))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('dist'))
-}, () => {
-  return gulp.src(['src/**/*', '!src/**/*.js'])
-    .pipe(gulp.dest('dist'))
-}))
+let compile = gulp.parallel(
+  () => {
+    return gulp.src('src/**/*.js')
+      .pipe(sourcemaps.init())
+      .pipe(bro({
+        transform: [
+          babelify.configure()
+        ]
+      }))
+      .pipe(sourcemaps.write('.'))
+      .pipe(gulp.dest('dist'))
+  },
+  () => {
+    return gulp.src(['src/**/*', '!src/**/*.js'])
+      .pipe(gulp.dest('dist'))
+  }
+)
+compile.description = 'Compile with babel into dist/'
+exports.compile = compile
 
-gulp.task('pack', gulp.parallel(() => {
-  return gulp.src(['dist/**/*', '!dist/**/*.map', 'node_modules/underscore/**/*', 'icons/**/clear-*.png', 'icons/**/*.min.svg', 'manifest.firefox.json', 'LICENSE'], { base: '.' })
-    .pipe(rename(path => {
-      if (path.basename === 'manifest.firefox') {
-        path.basename = 'manifest'
-      }
-    }))
-    .pipe(zip('tab-counter.firefox.zip'))
-    .pipe(gulp.dest('build'))
-}, () => {
-  return gulp.src(['dist/**/*.js', 'dist/**/*.html', 'node_modules/webextension-polyfill/dist/browser-polyfill.js', 'node_modules/underscore/underscore.js', 'icons/**/*.png', 'icons/**/*.min.svg', 'manifest.opera.json', 'LICENSE'], { base: '.' })
-    .pipe(rename(path => {
-      if (path.basename === 'manifest.opera') {
-        path.basename = 'manifest'
-      }
-    }))
-    .pipe(zip('tab-counter.opera.zip'))
-    .pipe(gulp.dest('build'))
-}))
+let pack = gulp.parallel(
+  () => {
+    return gulp.src(['dist/**/*', '!dist/**/*.map', 'node_modules/underscore/**/*', 'icons/**/clear-*.png', 'icons/**/*.min.svg', 'manifest.firefox.json', 'LICENSE'], { base: '.' })
+      .pipe(rename(path => {
+        if (path.basename === 'manifest.firefox') {
+          path.basename = 'manifest'
+        }
+      }))
+      .pipe(zip('tab-counter.firefox.zip'))
+      .pipe(gulp.dest('build'))
+  },
+  () => {
+    return gulp.src(['dist/**/*.js', 'dist/**/*.html', 'node_modules/webextension-polyfill/dist/browser-polyfill.js', 'node_modules/underscore/underscore.js', 'icons/**/*.png', 'icons/**/*.min.svg', 'manifest.opera.json', 'LICENSE'], { base: '.' })
+      .pipe(rename(path => {
+        if (path.basename === 'manifest.opera') {
+          path.basename = 'manifest'
+        }
+      }))
+      .pipe(zip('tab-counter.opera.zip'))
+      .pipe(gulp.dest('build'))
+  }
+)
+pack.description = 'Create a source archive in build/ for Firefox and Opera'
+exports.pack = pack
 
-gulp.task('watch', gulp.series('checkSafe', 'compile', () => {
-  return gulp.watch(['src/**/*', 'node_modules/webextension-polyfill/**/*', 'node_modules/underscore/**/*', 'icons/**/*', 'manifest.json', 'package.json'], gulp.parallel('checkSafe', 'compile'))
-}))
+let watch = gulp.series(lintSafe, compile,
+  () => {
+    return gulp.watch(
+      ['src/**/*', 'node_modules/webextension-polyfill/**/*', 'node_modules/underscore/**/*', 'icons/**/*', 'manifest.json', 'package.json'],
+      gulp.parallel(lintSafe, compile)
+    )
+  }
+)
+watch.description = 'Watch source files (and modules) for changes and rebuild'
+exports.watch = watch
 
-gulp.task('dist', gulp.series('static', 'check', 'clean', 'compile'))
-gulp.task('build', gulp.series('dist', 'pack'))
-gulp.task('default', gulp.series('build'))
+let dist = gulp.series(trim, lint, clean, compile)
+let build = gulp.series(dist, pack)
+
+exports.dist = dist
+exports.build = build
+exports.default = build
